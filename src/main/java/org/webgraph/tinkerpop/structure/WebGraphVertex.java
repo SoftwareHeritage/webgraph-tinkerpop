a5f8f7679228f55ba524968fccbd874fdf5d8c4d
Andrey <andreystar2403@gmail.com>
2022-01-05 22:14:52 +0300

    Query only a single direction of vertices, if a single direction is specified.

--- src/main/java/org/webgraph/tinkerpop/structure/WebGraphVertex.java
+++ src/main/java/org/webgraph/tinkerpop/structure/WebGraphVertex.java
@@ -19,38 +19,41 @@ public class WebGraphVertex extends WebGraphElement implements Vertex {
 
     @Override
     public Iterator<Vertex> vertices(Direction direction, String... edgeLabels) {
-        LazyLongIterator successors = graph.getBaseGraph().successors((Long) id());
-        Iterator<Vertex> out = Stream.generate(successors::nextLong)
-                                     .takeWhile(s -> s != -1)
-                                     .map(s -> (Vertex) new WebGraphVertex(s, graph))
-                                     .iterator();
-        LazyLongIterator predecessors = graph.getBaseGraph().predecessors((Long) id());
-        Iterator<Vertex> in = Stream.generate(predecessors::nextLong)
-                                    .takeWhile(s -> s != -1)
-                                    .map(s -> (Vertex) new WebGraphVertex(s, graph))
-                                    .iterator();
         switch (direction) {
             case OUT:
-                return out;
+                return toIterator(graph.getBaseGraph().successors((Long) id()));
             case IN:
-                return in;
+                return toIterator(graph.getBaseGraph().predecessors((Long) id()));
             default:
+                LazyLongIterator successors = graph.getBaseGraph().successors((Long) id());
+                Iterator<Vertex> out = toIterator(successors);
+                LazyLongIterator predecessors = graph.getBaseGraph().predecessors((Long) id());
+                Iterator<Vertex> in = toIterator(predecessors);
                 return IteratorUtils.concat(out, in);
         }
     }
 
+    private Iterator<Vertex> toIterator(LazyLongIterator source) {
+        return Stream.generate(source::nextLong)
+                     .takeWhile(s -> s != -1)
+                     .map(s -> (Vertex) new WebGraphVertex(s, graph))
+                     .iterator();
+    }
+
     @Override
     public Iterator<Edge> edges(Direction direction, String... edgeLabels) {
-        Iterator<Vertex> in = vertices(Direction.IN, edgeLabels);
-        Iterator<Edge> inE = IteratorUtils.map(in, from -> new WebGraphEdge((long) from.id(), (long) id(), graph));
-        Iterator<Vertex> out = vertices(Direction.OUT, edgeLabels);
-        Iterator<Edge> outE = IteratorUtils.map(out, to -> new WebGraphEdge((long) id(), (long) to.id(), graph));
         switch (direction) {
             case OUT:
-                return outE;
+                Iterator<Vertex> out = vertices(Direction.OUT, edgeLabels);
+                return IteratorUtils.map(out, to1 -> new WebGraphEdge((long) id(), (long) to1.id(), graph));
             case IN:
-                return inE;
+                Iterator<Vertex> in = vertices(Direction.IN, edgeLabels);
+                return IteratorUtils.map(in, from1 -> new WebGraphEdge((long) from1.id(), (long) id(), graph));
             default:
+                Iterator<Vertex> in2 = vertices(Direction.IN, edgeLabels);
+                Iterator<Edge> inE = IteratorUtils.map(in2, from -> new WebGraphEdge((long) from.id(), (long) id(), graph));
+                Iterator<Vertex> out2 = vertices(Direction.OUT, edgeLabels);
+                Iterator<Edge> outE = IteratorUtils.map(out2, to -> new WebGraphEdge((long) id(), (long) to.id(), graph));
                 return IteratorUtils.concat(outE, inE);
         }
     }
